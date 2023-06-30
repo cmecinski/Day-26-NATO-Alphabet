62143d5ed9212d85d57f41421db892491e26451d
Chris <cmecinski@gmail.com>
2023-06-29 20:00:31 -0500

    Added Exception Handling

--- main.py
+++ main.py
@@ -11,8 +11,20 @@ nato_data = pandas.read_csv("nato_phonetic_alphabet.csv")
 
 nato_dictionary = {row.letter: row.code for (index, row) in nato_data.iterrows()}
 
-user_input = input("Enter a word: ")
-user_list = list(user_input.upper())
-output = [nato_dictionary[letter] for letter in user_list]
 
-print(output)
+def generate_phonetic():
+    user_input = input("Enter a word: ")
+    try:
+        user_list = list(user_input.upper())
+        output = [nato_dictionary[letter] for letter in user_list]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generate_phonetic()
+    else:
+        print(output)
+
+
+generate_phonetic()
+
+
+
